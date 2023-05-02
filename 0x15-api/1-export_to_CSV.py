0cbd636ffd90085993c9d9a72901070a07a0bde2
Mamurooyiboluawhore <111292261+Mamurooyiboluawhore@users.noreply.github.com>
2023-05-02 20:16:12 +0100

    Update 1-export_to_CSV.py

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -19,4 +19,4 @@ if __name__ == "__main__":
     # opening with csv
     with open("{}.csv".format(id_), 'w', newline='') as file:
         writer = csv.writer(file, quoting=csv.QUOTE_ALL)
-        writer.writerows(utask)
+        writer.writerows(utask).
