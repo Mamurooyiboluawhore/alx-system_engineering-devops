84fadf6a0b6c2c34ba19c643c54fbc07d7f86e60
Mamuro oyiboluawhore <missmarvelous2016@gmail.com>
2023-05-12 02:16:02 +0000

    task 2

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -8,13 +8,12 @@ import requests
 def recurse(subreddit, hot_list=[], after=""):
     url = 'https://www.reddit.com/r/{}/hot.json?limit=10'.format(subreddit)
     headers = {'User-Agent': 'Mamuro'}
-    params = {"after": "after"}
+    params = {"after": "after", "limit": 100}
     response = requests.get(url, headers=headers, allow_redirects=False)
     if response.status_code == 200:
-        response = response.json().get('data')
-        after = response.get("after")
-        hotPost = response.get('children')
-        for post in hotPost:
+        hotPost = response.json().get('data')
+        after = hotPost.get("after")
+        for post in hotPost.get('children'):
             hot_list.append(post.get('data').get('title'))
 
             if after:
