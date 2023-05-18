31ab4599dc5446d22109f7912d4c5790cd7930bf
Mamurooyiboluawhore <missmarvelous2016@gmail.com>
2023-05-18 22:01:43 +0000

    puppet

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,10 +1,9 @@
 # a puppet script that changes ulimit
-
 exec { '/etc/default/nginx':
   command => "sed -i 's/15/3028/g' /etc/default/nginx",
-  path => "/usr/local/bin/:/bin/"
+  path => "/usr/local/bin/:/bin/",
 }
 exec { 'nginx_restart':
   command => "nginx restart",
-  path => "/etc/init.d/"
+  path => "/etc/init.d/",
 }  
